b36117560b4129c26a836f47dc54986384629b9f
Prabhav <prabhav.tewari@siemens.com>
2023-05-03 02:26:58 +0530

    webcam select

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,76 @@
-import { useState } from 'react'
+import { useState,useRef,useEffect } from 'react'
 import './App.css'
-import { Row, Container, Col, Form } from "react-bootstrap"
+import { Row, Container, Col, Form,Button } from "react-bootstrap"
 import hero_img from "./assets/hero-img.png"
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const vidRef = useRef()
+  var videoSelect = null;
+  var videoElement = useRef();
+
+  useEffect(()=>{
+    if(!vidRef.current) return
+    videoSelect=vidRef.current
+
+  },[vidRef])
+
+
+
+  
+
+  function getDevices() {
+    // AFAICT in Safari this only gets default devices until gUM is called :/
+    return navigator.mediaDevices.enumerateDevices();
+  }
+
+  function gotDevices(deviceInfos) {
+    window.deviceInfos = deviceInfos; // make available to console
+    console.log('Available input and output devices:', deviceInfos);
+    for (const deviceInfo of deviceInfos) {
+      const option = document.createElement('option');
+      option.value = deviceInfo.deviceId;
+      if (deviceInfo.kind === 'videoinput') {
+        option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+        videoSelect.appendChild(option);
+      }
+    }
+  }
+
+  function getStream() {
+    if (window.stream) {
+      window.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+
+    const videoSource = videoSelect.value;
+    const constraints = {
+      video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    };
+    return navigator.mediaDevices.getUserMedia(constraints).
+      then(gotStream).catch(handleError);
+  }
+
+  function gotStream(stream) {
+    window.stream = stream; // make stream available to console
+    videoSelect.selectedIndex = [...videoSelect.options].
+      findIndex(option => option.text === stream.getVideoTracks()[0].label);
+    videoElement.current.srcObject = stream;
+    videoElement.current.style.display = "inline-block"
+
+  }
+
+  function handleError(error) {
+    console.error('Error: ', error);
+  }
+  function requestAccess(e){
+    e.currentTarget.style.display = "none"
+    videoSelect.style.display="block"
+    videoSelect.onchange = getStream;
+    videoSelect.onClick = getStream().then(getDevices).then(gotDevices);
+  }
 
   return (
     <div className="App">
@@ -18,7 +83,7 @@ function App() {
               </div>
             </Col>
             <Col>
-              <img className='hero-img' src={hero_img} alt="" srcset="" />
+              <img className='hero-img' src={hero_img} alt=""  />
             </Col>
           </Row>
         </Container>
@@ -36,10 +101,14 @@ function App() {
             <Col className='select-camera mx-3 my-3 py-3 px-3 rounded'>
               <Form.Group controlId="formFile" className="mb-3">
                 <Form.Label className='h3 my-3'>Select Camera</Form.Label>
-                <Form.Control type="file" />
+                <div >
+                <Button variant="primary w-100" onClick={(e) => requestAccess(e)}>Select Camera</Button>
+                  <select id="videoSource" ref={vidRef} className='form-select'></select>
+                  </div>
               </Form.Group>
             </Col>
           </Row>
+          <video autoPlay muted playsInline ref={videoElement}></video>
         </Container>
       </section>
       {/* <Container>
